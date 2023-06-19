2b412c821c090cd1a625453b41b32a94d640c26a
RemieJanssen <27146369+RemieJanssen@users.noreply.github.com>
2023-06-19 08:01:06 +0200

    Start package (#1)
    * DiNetwork class cleanup and tests
    
    * clean up basic rearrangement moves and add tests
    
    * restructure and add tests for isomorphism
    
    * test is_orchard is_binary is_tree_child tree-based
    
    * Implement and test vertical moves
    
    * add cov to pytest config
    
    * add count_automorphisms
    
    * clean up mcmc generator with tests
    
    * add changelog

---
+++ tests/isomorphism/test_isomorphism.py
@@ -0,0 +1,127 @@
+import unittest
+
+from phylox import DiNetwork, LABEL_ATTR
+from phylox.isomorphism import is_isomorphic, count_automorphisms
+
+class TestIsomorphism(unittest.TestCase):
+    def test_isomorphism(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+        )
+        self.assertTrue(is_isomorphic(network1, network2))
+
+    def test_isomorphism_with_labels(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        self.assertTrue(is_isomorphic(network1, network2))
+
+    def test_isomorphism_with_switched_labels(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(6, "a"), (5, "b")],
+        )
+        self.assertFalse(is_isomorphic(network1, network2))
+
+    def test_isomorphism_with_partial_isomorphism(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        self.assertTrue(is_isomorphic(network1, network2, partial_isomorphism=[(1, 1)]))
+    
+    def test_isomorphism_with_partial_isomorphism_on_leaves(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        self.assertTrue(is_isomorphic(network1, network2, partial_isomorphism=[(5, 5)]))
+
+    def test_isomorphism_with_partial_isomorphism_on_leaves_swapped(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        self.assertFalse(is_isomorphic(network1, network2, partial_isomorphism=[(3, 4), (4, 3)]))
+
+    def test_isomorphism_with_partial_isomorphism_on_leaves_swapped2(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        self.assertFalse(is_isomorphic(network1, network2, partial_isomorphism=[(6, 5), (5, 6)]))
+
+    def test_isomorphism_ignore_labels(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        self.assertTrue(is_isomorphic(network1, network2, ignore_labels=True))
+        
+    def test_isomorphism_ignore_labels_partial_isom(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 4), (3, 5), (4, 6)],
+            labels=[(5, "a"), (6, "b")],
+        )
+        self.assertFalse(is_isomorphic(network1, network2, ignore_labels=True, partial_isomorphism=[(6, 5), (5, 6)]))
+
+class TestAutomorphism(unittest.TestCase):
+    def test_automorphism_simple(self):
+        network = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4)],
+            labels=[(3, "a"), (4, "b")],
+        )
+        self.assertEqual(count_automorphisms(network), 1)
+        self.assertEqual(count_automorphisms(network, ignore_labels=True), 2)
+
+    def test_automorphism_larger(self):
+        network = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 5), (3, 6), (4, 5), (4, 6), (5, 7), (6, 8)],
+            labels=[(7, "a"), (8, "b")],
+        )
+        self.assertEqual(count_automorphisms(network), 2)
+        self.assertEqual(count_automorphisms(network, ignore_labels=True), 4)
+
+    def test_automorphism_larger_strange_labels(self):
+        network = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (3, 5), (3, 6), (4, 5), (4, 6), (5, 7), (6, 8)],
+            labels=[(7, "a"), (8, "a")],
+        )
+        self.assertEqual(count_automorphisms(network), 4)
+        self.assertEqual(count_automorphisms(network, ignore_labels=True), 4)
---
+++ tests/rearrangement/test_move.py
@@ -0,0 +1,136 @@
+import unittest
+import pytest
+
+from phylox import DiNetwork
+from phylox.exceptions import InvalidMoveDefinitionException, InvalidMoveException
+from phylox.rearrangement.move import Move, apply_move, apply_move_sequence
+from phylox.rearrangement.movetype import MoveType
+
+class TestMoveClass(unittest.TestCase):
+    def test_invalid_move_rspr(self):
+        with pytest.raises(
+            InvalidMoveDefinitionException,
+            match="rSPR moves must be defined as moves of type tail or head.",
+        ):
+            Move(
+                origin=(0, 1),
+                moving_edge=(2, 3),
+                target=(4, 5),
+                move_type=MoveType.RSPR,
+            )
+
+    def test_invalid_move_missing_move_type(self):
+        with pytest.raises(InvalidMoveDefinitionException, match="Missing move_type."):
+            Move(origin=(0, 1), moving_edge=(2, 3), target=(4, 5))
+
+    def test_invalid_move_missing_arg(self):
+        with pytest.raises(
+            InvalidMoveDefinitionException,
+            match="Missing one of origin, moving_edge, or target.",
+        ):
+            Move(origin=(0, 1), moving_edge=(2, 3), move_type=MoveType.TAIL)
+
+    def test_invalid_move_target_equals_moving_edge(self):
+        with pytest.raises(
+            InvalidMoveDefinitionException,
+            match="Moving edge must not be the target edge.",
+        ):
+            Move(
+                origin=(0, 1),
+                moving_edge=(2, 3),
+                target=(2, 3),
+                move_type=MoveType.TAIL,
+            )
+
+    def test_make_move_head(self):
+        m = Move(
+            origin=(0, 1),
+            moving_edge=(2, 3),
+            target=(4, 5),
+            move_type=MoveType.HEAD,
+        )
+        assert m.move_type == MoveType.HEAD
+
+    def test_make_move_tail(self):
+        m = Move(
+            origin=(0, 1),
+            moving_edge=(2, 3),
+            target=(4, 5),
+            move_type=MoveType.TAIL,
+        )
+        assert m.move_type == MoveType.TAIL
+
+    def test_make_move_none(self):
+        m = Move(
+            move_type=MoveType.NONE,
+        )
+        assert m.move_type == MoveType.NONE
+
+    def test_make_move_vplu(self):
+        m = Move(
+            move_type=MoveType.VPLU,
+            start_edge=(0, 1),
+            end_edge=(2, 3),
+            start_node=5,
+            end_node=6,
+        )
+        assert m.move_type == MoveType.VPLU
+
+    def test_make_move_vplu_infer_new_nodes(self):
+        network = DiNetwork(
+            edges=[(0, 1), (1, 2), (1, 3)],
+        )
+        m = Move(
+            move_type=MoveType.VPLU,
+            start_edge=(1, 2),
+            end_edge=(1, 3),
+            network=network,
+        )
+        assert m.move_type == MoveType.VPLU
+
+    def test_make_move_vplu_infer_new_nodes_invalid(self):
+        network = DiNetwork(
+            edges=[(0, 1), (1, 2), (1, 3)],
+        )
+        with pytest.raises(
+            InvalidMoveDefinitionException,
+            match="Either a start_node and end_node, or a network must be given.",
+        ):
+            Move(
+                move_type=MoveType.VPLU,
+                start_edge=(1, 2),
+                end_edge=(1, 3),
+            )
+
+    def test_random_move(self):
+        network = DiNetwork(
+            edges=[(0, 1), (1, 2), (1, 3)],
+        )
+        for i in range(100):
+            try:
+                m = Move.random_move(network)
+            except InvalidMoveDefinitionException:
+                return
+            assert m.move_type in [
+                MoveType.HEAD,
+                MoveType.TAIL,
+                MoveType.VMIN,
+                MoveType.VPLU,
+            ]
+
+    def test_random_move_tail(self):
+        network = DiNetwork(
+            edges=[(0, 1), (1, 2), (1, 3)],
+        )
+        for i in range(100):
+            try:
+                m = Move.random_move(
+                    network,
+                    move_type_probabilities={MoveType.TAIL: 1.0},
+                )
+            except InvalidMoveDefinitionException:
+                continue
+            self.assertIn(m.move_type, [
+                MoveType.TAIL,
+            ])
+            self.assertIn(m.moving_edge, network.edges)
