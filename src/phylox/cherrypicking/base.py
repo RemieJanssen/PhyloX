9b42f7afba1bacec4b80129986d157c22ae68345
RemieJanssen <remiejanssen92@gmail.com>
2023-06-25 12:24:02 +0200

    tcs WIP (labels not handled correctly)

--- src/phylox/cherrypicking/base.py
+++ src/phylox/cherrypicking/base.py
@@ -8,6 +8,55 @@ class CHERRYTYPE(Enum):
     NONE = 0
 
 
+def find_reducible_pairs_with_second(N, x):
+    """
+    Finds a list of reducible pairs (cherries and reticulated cherries) in the 
+    network N with leaf x as second element of the pair.
+    """
+    if not N.is_leaf(x):
+        raise ValueError("x must be a leaf of N")
+
+    parent = N.parent(x)
+    if N.in_degree(parent) == 0:
+        return []
+
+    reducible_pairs = list()
+    for sibling in N.successors(parent):
+        if sibling == x:
+            continue
+        sibling_out_degree = N.out_degree(sibling)
+        if sibling_out_degree == 0:
+            reducible_pairs.append((sibling, x))
+        if sibling_out_degree == 1:
+            for sibling_child in N.successors(sibling):
+                if N.out_degree(sibling_child) == 0:
+                    reducible_pairs.append((sibling_child, x))
+    return reducible_pairs
+
+
+def find_reticulated_cherry_with_first(N, x):
+    """
+    Finds a list of reticulated cherries in the network N with leaf x as first
+    element of the pair.
+    """
+
+    if not N.is_leaf(x):
+        raise ValueError("x must be a leaf of N")
+
+    parent = N.parent(x)
+    if parent is None:
+        return []
+    if not N.is_reticulation(parent):
+        return []
+
+    reticulated_cherries = list()
+    for pp in N.predecessors(parent):
+        for ppc in N.successors(pp):
+            if ppc == parent or not N.is_leaf(ppc):
+                continue
+            reticulated_cherries.append((x, ppc))
+    return reticulated_cherries
+
 def is_second_in_reducible_pair(network, x):
     for node in network.predecessors(x):
         px = node
@@ -25,7 +74,7 @@ def is_second_in_reducible_pair(network, x):
 def reduce_pair(network, x, y):
     network = deepcopy(network)
 
-    cherry_type = check_cherry(network, x, y)
+    cherry_type = check_reducible_pair(network, x, y)
     if cherry_type == CHERRYTYPE.CHERRY:
         px = network.parent(x)
         network.remove_node(x)
@@ -48,7 +97,7 @@ def reduce_pair(network, x, y):
     return network, cherry_type
 
 
-def check_cherry(network, x, y):
+def check_reducible_pair(network, x, y):
     if network.has_node(x):
         if network.has_node(y):
             for px in network.predecessors(x):
---
+++ src/phylox/cherrypicking/tree_child_sequences.py
@@ -0,0 +1,38 @@
+import ast
+import itertools
+import random
+import time
+
+import networkx as nx
+from phylox.cherrypicking import CHERRYTYPE, check_reducible_pair, reduce_pair, find_reducible_pairs_with_second, find_reticulated_cherry_with_first
+from phylox.classes.dinetwork import is_tree_child
+
+def find_tree_child_sequence(N):
+    reducible_pairs = list()
+    for x in N.leaves:
+        reducible_pairs.extend(find_reducible_pairs_with_second(N, x))
+    tree_child_sequence = list()
+    while reducible_pairs:
+        pair = reducible_pairs.pop()
+        cherry_type = check_reducible_pair(N, *pair)
+        if not cherry_type == CHERRYTYPE.NONE:
+            N, _ = reduce_pair(N, *pair)
+            tree_child_sequence.append(pair)
+            reducible_pairs.extend(find_reducible_pairs_with_second(N, pair[1]))
+            reducible_pairs.extend(find_reticulated_cherry_with_first(N, pair[1]))
+    print(tree_child_sequence)
+    return tree_child_sequence
+
+
+def check_cherry_picking_sequence(N, cherry_picking_sequence):
+    for pair in cherry_picking_sequence:
+        N, _ = reduce_pair(N, *pair)
+    if N.size() == 1:
+        return True
+    return False
+
+
+def tree_child_network_contains(N, M):
+    if not is_tree_child(N):
+        raise ValueError("N must be a tree child network")
+    return check_cherry_picking_sequence(M, find_tree_child_sequence(N))
