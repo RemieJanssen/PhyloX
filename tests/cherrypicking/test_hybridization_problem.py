a7d68b11d267965b813f8e6640a6e8d046e0a69a
RemieJanssen <remiejanssen92@gmail.com>
2023-07-08 22:11:25 +0200

    added more tests to fix

--- tests/cherrypicking/test_hybridization_problem.py
+++ tests/cherrypicking/test_hybridization_problem.py
@@ -14,3 +14,45 @@ class TestHybridizationProblem(unittest.TestCase):
         result = problem.CPSBound(progress=True)
         print(result)
         self.assertEqual(len(result), 1)
+
+    def test_two_trees_isomorphic(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4)],
+            labels=[(3, "A"), (4, "B")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 6), (2, 7)],
+            labels=[(6, "A"), (7, "B")],
+        )
+        problem = HybridizationProblem([network1, network2], newick_strings=False)
+        result = problem.CPSBound(progress=True)
+        print(result)
+        self.assertEqual(len(result), 1)
+
+    def test_two_trees_different_but_trivial(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4)],
+            labels=[(3, "A"), (4, "B")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 6), (2, 7)],
+            labels=[(6, "C"), (7, "D")],
+        )
+        problem = HybridizationProblem([network1, network2], newick_strings=False)
+        result = problem.CPSBound(progress=True)
+        print(result)
+        self.assertEqual(len(result), 2)
+
+    def test_two_trees_different_nontrivial(self):
+        network1 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (4, 5), (4, 6)],
+            labels=[(3, "A"), (5, "B"), (6, "C")],
+        )
+        network2 = DiNetwork(
+            edges=[(1, 2), (2, 3), (2, 4), (4, 5), (4, 6)],
+            labels=[(5, "A"), (3, "B"), (6, "C")],
+        )
+        problem = HybridizationProblem([network1, network2], newick_strings=False)
+        result = problem.CPSBound(progress=True)
+        print(result)
+        self.assertEqual(len(result), 3)
